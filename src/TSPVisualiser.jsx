3234bcbbb5568e58019841dd9f8f12f958869eb2
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2020-03-10 06:10:26 +1000

    canvas and point drawing basic implementation

--- src/TSPVisualiser.jsx
+++ src/TSPVisualiser.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { random } from "lodash";
 
 const ANIMATION_SPEED = 1;
 const NUM_POINTS = 20;
+const CANVAS_HEIGHT = 500;
+const CANVAS_WIDTH = 500;
 
 const POINT_COLOUR = "blue";
 const PATH_COLOUR = "light blue";
@@ -13,6 +14,7 @@ export default class TSPVisualiser extends React.Component {
   constructor(props) {
     super(props);
 
+    this.myCanvas = React.createRef();
     this.state = {
       cities: []
     };
@@ -24,9 +26,11 @@ export default class TSPVisualiser extends React.Component {
 
   resetArray() {
     const cities = [];
+    this.ctx.clearRect(0, 0, CANVAS_HEIGHT, CANVAS_WIDTH);
     for (let i = 0; i < NUM_POINTS; i++) {
       cities.push(randomCoords(0, 500));
     }
+    console.log(cities);
     this.setState({ cities });
   }
 
@@ -35,11 +39,21 @@ export default class TSPVisualiser extends React.Component {
 
   christofidesAlg() {}
 
+  componentDidMount() {
+    const canvas = this.refs.myCanvas;
+    this.ctx = canvas.getContext("2d");
+  }
   render() {
     const { cities } = this.state;
-    console.log(cities);
+    cities.map(value => drawCoordinates(this.ctx, value));
+
     return (
       <div className="pointContainer">
+        <canvas
+          ref="myCanvas"
+          height={CANVAS_HEIGHT}
+          width={CANVAS_WIDTH}
+        ></canvas>
         <button onClick={() => this.resetArray()}>Generate New Array</button>
         <button onClick={() => this.testAlg()}>Test Algorithm</button>
       </div>
@@ -50,11 +64,18 @@ export default class TSPVisualiser extends React.Component {
 function randomCoords(min, max) {
   let x = Math.floor(Math.random() * (max - min + 1) + min);
   let y = Math.floor(Math.random() * (max - min + 1) + min);
-  return { x, y };
+  return [x, y];
 }
 
 function distance(a, b) {
   const deltaX = a.x - b.x;
   const deltaY = a.y - b.y;
   return Math.hypot(deltaX, deltaY);
+}
+
+function drawCoordinates(canvas, coords) {
+  canvas.fillStyle = POINT_COLOUR;
+  canvas.beginPath();
+  canvas.arc(coords[0], coords[1], 5, 0, 2 * Math.PI, true);
+  canvas.fill();
 }
