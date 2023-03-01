708a165c6dab2a4a4b00f7cda245fa00256c9c6d
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2023-03-01 23:52:38 +1000

    Fixed NearestInsertion, Added CheapestInsertion, changed seeking line to accomodate multiple active cities, remove readme

--- src/algorithms.js
+++ src/algorithms.js
@@ -1,118 +1,219 @@
 function nearestNeighbour(cities) {
-    let route = []
-    let dist = Infinity;
-    let bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  let route = [];
+  let dist = Infinity;
+  // let bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  let bestIdx = 0;
 
-    let counter = 0;
-    while (cities.length > 0) {
-        route.push({ visited: [], active: [], seeking: [], unvisited: [] })
-        if (counter > 0) {
-            route[counter]["visited"] = route[counter - 1]["visited"].concat([cities[bestIdx]]);
-        } else {
-            route[counter]["visited"].push(cities[bestIdx]);
-        }
-
-        let minDist = Infinity
-        let active = cities[bestIdx];
-        route[counter]["active"].push(cities[bestIdx]);
-        cities.splice(bestIdx, 1);
-        route[counter]["unvisited"] = route[counter]["unvisited"].concat(cities);
-        route[counter]["seeking"].push([]);
-
-        for (let i = 0; i < cities.length; i++) {
-            route[counter]["seeking"][0].push([cities[i]]);
-            dist = distance(active, cities[i]);
-            if (dist < minDist) {
-                minDist = dist;
-                bestIdx = i;
-            }
-        }
-
-        counter++;
+  let counter = 0;
+  while (cities.length > 0) {
+    route.push({ visited: [], active: [], seeking: [], unvisited: [] });
+    if (counter > 0) {
+      route[counter]["visited"] = route[counter - 1]["visited"].concat([
+        cities[bestIdx],
+      ]);
+    } else {
+      route[counter]["visited"].push(cities[bestIdx]);
     }
-    route.push({ visited: [], active: [], seeking: [], unvisited: [] })
-    route[counter]["visited"] = route[counter - 1]["visited"].concat([])
-    return route;
-}
 
+    let minDist = Infinity;
+    let active = cities[bestIdx];
+    route[counter]["active"].push([cities[bestIdx]]);
+    cities.splice(bestIdx, 1);
+    route[counter]["unvisited"] = route[counter]["unvisited"].concat(cities);
+    route[counter]["seeking"].push([]);
+
+    for (let i = 0; i < cities.length; i++) {
+      route[counter]["seeking"][0].push([cities[i]]);
+      dist = distance(active, cities[i]);
+      if (dist < minDist) {
+        minDist = dist;
+        bestIdx = i;
+      }
+    }
+
+    counter++;
+  }
+  route.push({ visited: [], active: [], seeking: [], unvisited: [] });
+  route[counter]["visited"] = route[counter - 1]["visited"].concat([]);
+  console.log(calculateTour(route[counter]["visited"]));
+  return route;
+}
 
 function nearestInsertion(cities) {
-    let route = [];
-    route.push({ visited: [], active: [], seeking: [], unvisited: [] })
-    let bestIdx = Math.floor(Math.random() * (cities.length - 1));
-    route[0]["visited"].push(cities[bestIdx])
-    cities.splice(bestIdx, 1);
+  let route = [];
+  route.push({ visited: [], active: [], seeking: [], unvisited: [] });
+  // let bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  let bestIdx = 0;
+  route[0]["visited"].push(cities[bestIdx]);
+  cities.splice(bestIdx, 1);
 
-    bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  // bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  bestIdx = 1;
+  route[0]["visited"].push(cities[bestIdx]);
+  cities.splice(bestIdx, 1);
 
-    route[0]["visited"].push(cities[bestIdx])
-    cities.splice(bestIdx, 1);
-
-    let counter = 0
-    let seekCounter = 0
-    while (cities.length > 0) {
-        let minDist = Infinity;
-        let dist = Infinity;
-        for (const city of route[counter]["visited"]) {
-            route[counter]["active"].push(city);
-            route[counter]["seeking"].push([]);
-            for (let i = 0; i <= cities.length - 1; i++) {
-                route[counter]["seeking"][seekCounter].push([cities[i]]);
-                dist = distance(city, cities[i])
-                if (dist < minDist) {
-                    minDist = dist;
-                    bestIdx = i;
-                }
-            }
-            seekCounter++;
+  let counter = 0;
+  let seekCounter = 0;
+  while (cities.length > 0) {
+    let minDist = Infinity;
+    let dist = Infinity;
+    let bestInsertion = 0;
+    for (let j = 0; j <= route[counter]["visited"].length - 1; j++) {
+      route[counter]["active"].push([route[counter]["visited"][j]]);
+      route[counter]["seeking"].push([]);
+      for (let i = 0; i <= cities.length - 1; i++) {
+        route[counter]["seeking"][seekCounter].push([cities[i]]);
+        dist = distance(route[counter]["visited"][j], cities[i]);
+        if (dist < minDist) {
+          minDist = dist;
+          bestIdx = i;
+          bestInsertion = j;
         }
-
-        let toAdd = cities[bestIdx]
-        route[counter]["active"].push(cities[bestIdx]);
-        route[counter]["unvisited"] = route[counter]["unvisited"].concat(cities)
-        cities.splice(bestIdx, 1);
-        route[counter]["seeking"].push([]);
-
-        minDist = Infinity;
-        dist = Infinity;
-        for (let i = 1; i <= route[counter]["visited"].length; i++) {
-            let next = i;
-            let prev = i - 1;
-
-            if (i === route[counter]["visited"].length) {
-                next = 0;
-            }
-
-            route[counter]["seeking"][seekCounter].push([route[counter]["visited"][prev], route[counter]["visited"][next]]);
-
-            dist = distance(route[counter]["visited"][prev], toAdd) +
-                distance(toAdd, route[counter]["visited"][next]) -
-                distance(route[counter]["visited"][prev], route[counter]["visited"][next]);
-
-            if (dist < minDist) {
-                minDist = dist;
-                bestIdx = next;
-            }
-        }
-        seekCounter = 0;
-
-        route.push({ visited: [], active: [], seeking: [], unvisited: [] });
-        counter++;
-
-        let temp = [...route[counter - 1]["visited"]];
-        temp.splice(bestIdx, 0, toAdd);
-        route[counter]["visited"] = route[counter]["visited"].concat(temp)
+      }
+      seekCounter++;
     }
-    return route;
+
+    let toAdd = cities[bestIdx];
+    let prev, next;
+    if (bestInsertion === 0) {
+      prev = route[counter]["visited"].length - 1;
+    } else {
+      prev = bestInsertion - 1;
+    }
+    if (bestInsertion === route[counter]["visited"].length - 1) {
+      next = 0;
+    } else {
+      next = bestInsertion + 1;
+    }
+    let beforeDist =
+      distance(route[counter]["visited"][bestInsertion], toAdd) +
+      distance(toAdd, route[counter]["visited"][next]) -
+      distance(
+        route[counter]["visited"][bestInsertion],
+        route[counter]["visited"][next]
+      );
+
+    let afterDist =
+      distance(route[counter]["visited"][prev], toAdd) +
+      distance(toAdd, route[counter]["visited"][bestInsertion]) -
+      distance(
+        route[counter]["visited"][prev],
+        route[counter]["visited"][bestInsertion]
+      );
+
+    if (beforeDist < afterDist) {
+      bestInsertion = next;
+    }
+
+    route[counter]["unvisited"] = route[counter]["unvisited"].concat(cities);
+    cities.splice(bestIdx, 1);
+
+    seekCounter = 0;
+
+    route.push({ visited: [], active: [], seeking: [], unvisited: [] });
+    counter++;
+
+    let temp = [...route[counter - 1]["visited"]];
+    temp.splice(bestInsertion, 0, toAdd);
+    route[counter]["visited"] = route[counter]["visited"].concat(temp);
+  }
+  console.log(calculateTour(route[counter]["visited"]));
+  return route;
 }
 
+function cheapestInsertion(cities) {
+  let route = [];
+  route.push({ visited: [], active: [], seeking: [], unvisited: [] });
+  // let bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  let bestIdx = 0;
+  route[0]["visited"].push(cities[bestIdx]);
+  cities.splice(bestIdx, 1);
+
+  // bestIdx = Math.floor(Math.random() * (cities.length - 1));
+  bestIdx = 1;
+  route[0]["visited"].push(cities[bestIdx]);
+  cities.splice(bestIdx, 1);
+
+  let counter = 0;
+  let seekCounter = 0;
+  while (cities.length > 0) {
+    let minDist = Infinity;
+    let dist = Infinity;
+    let bestInsertion = 0;
+    // Find every adjacent city and set them as active pairs
+    for (let j = 0; j <= route[counter]["visited"].length - 1; j++) {
+      if (j === route[counter]["visited"].length - 1) {
+        route[counter]["active"].push([
+          route[counter]["visited"][j],
+          route[counter]["visited"][0],
+        ]);
+      } else {
+        route[counter]["active"].push([
+          route[counter]["visited"][j],
+          route[counter]["visited"][j + 1],
+        ]);
+      }
+    }
+
+    // console.log(route[counter]);
+    // For each unvisited city calculate tour distance against all active pairs
+    for (let j = 0; j <= route[counter]["active"].length - 1; j++) {
+      route[counter]["seeking"].push([]);
+      for (let i = 0; i <= cities.length - 1; i++) {
+        route[counter]["seeking"][seekCounter].push([cities[i]]);
+        dist =
+          distance(route[counter]["active"][j][0], cities[i]) +
+          distance(cities[i], route[counter]["active"][j][1]) -
+          distance(
+            route[counter]["active"][j][0],
+            route[counter]["active"][j][1]
+          );
+        // console.log(route[counter]["active"][j][0]);
+        // console.log(route[counter]["active"][j][1]);
+        // console.log(cities[i]);
+        // console.log(dist);
+        if (dist < minDist) {
+          minDist = dist;
+          bestIdx = i;
+          bestInsertion = j + 1;
+        }
+      }
+      seekCounter++;
+    }
+    // break;
+    route[counter]["unvisited"] = route[counter]["unvisited"].concat(cities);
+
+    seekCounter = 0;
+
+    route.push({ visited: [], active: [], seeking: [], unvisited: [] });
+    counter++;
+
+    let temp = [...route[counter - 1]["visited"]];
+    temp.splice(bestInsertion, 0, cities[bestIdx]);
+    cities.splice(bestIdx, 1);
+    route[counter]["visited"] = route[counter]["visited"].concat(temp);
+  }
+  // console.log(route);
+  console.log(calculateTour(route[counter]["visited"]));
+  return route;
+}
+
+function calculateTour(cities) {
+  let totalDistance = 0;
+  for (let i = 0; i < cities.length; i++) {
+    if (i === cities.length - 1) {
+      totalDistance += distance(cities[0], cities[i]);
+    } else {
+      totalDistance += distance(cities[i], cities[i + 1]);
+    }
+  }
+  return totalDistance;
+}
 
 function distance(a, b) {
-    const deltaX = a.x - b.x;
-    const deltaY = a.y - b.y;
-    return Math.sqrt(deltaX ** 2 + deltaY ** 2);
+  const deltaX = a.x - b.x;
+  const deltaY = a.y - b.y;
+  return Math.sqrt(deltaX ** 2 + deltaY ** 2);
 }
 
-export {
-    nearestNeighbour, nearestInsertion
-};
+export { nearestNeighbour, nearestInsertion, cheapestInsertion };
--- src/app.jsx
+++ src/app.jsx
@@ -1,174 +1,237 @@
 import React from "react";
 import LineTo from "react-lineto";
-import { Slider } from 'rsuite';
+import { Slider } from "rsuite";
 
-import { nearestNeighbour, nearestInsertion } from "./algorithms";
-import City, { createCities } from "./city"
+import {
+  nearestNeighbour,
+  nearestInsertion,
+  cheapestInsertion,
+} from "./algorithms";
+import City, { createCities } from "./city";
 
 import "rsuite/dist/rsuite.min.css";
-import "./css/app.css"
+import "./css/app.css";
 
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            cities: [],
-            lines: [],
-            seeking: [{className: "none"}, [{className: "none"}]],
-            ANIMATION_DELAY: 50,
-            NUM_POINTS: 20
-        };
+    this.state = {
+      cities: [],
+      lines: [],
+      seeking: [[{ className: "none" }], [{ className: "none" }]],
+      ANIMATION_DELAY: 50,
+      NUM_POINTS: 20,
+    };
+  }
+
+  begin() {
+    const coords = [];
+
+    for (let i = 0; i < this.state.NUM_POINTS; i++) {
+      coords.push(randomCoords(140, 700));
     }
 
-    begin() {
-        const coords = [];
+    const cities = createCities(coords);
+    this.setState({ cities: cities, lines: [] });
+  }
 
-        for (let i = 0; i < this.state.NUM_POINTS; i++) {
-            coords.push(randomCoords(140, 700));
+  componentDidMount() {
+    this.begin();
+  }
+
+  reset(cities) {
+    cities.map((city) => {
+      city.isRoute = false;
+    });
+
+    this.setState({ cities: cities, lines: [] });
+  }
+
+  async displayPath(route) {
+    // console.log(route);
+    for (const step of route.slice(0, -1)) {
+      let visited = step["visited"];
+
+      visited.forEach((e) => {
+        e.isRoute = true;
+      });
+
+      let unvisited = step["unvisited"];
+      this.setState({ cities: visited.concat(unvisited), lines: visited });
+
+      for (let i = 0; i < step["active"].length; i++) {
+        // console.log(step['active'][i])
+        step["active"][i].isActive = true;
+        step["active"][i].isRoute = false;
+        for (let j = 0; j < step["seeking"][i].length; j++) {
+          //   console.log(step["active"][i]);
+          //   console.log(step["seeking"]);
+          this.setState({
+            seeking: [step["active"][i], step["seeking"][i][j]],
+          });
+          await timer(this.state.ANIMATION_DELAY);
         }
-
-        const cities = createCities(coords);
-        this.setState({ cities: cities, lines: [] });
+        step["active"][i].isActive = false;
+        step["active"][i].isRoute = true;
+      }
     }
 
-    componentDidMount() {
-        this.begin();
-    }
+    this.setState({
+      cities: route[route.length - 1]["visited"],
+      seeking: [[{ className: "none" }], [{ className: "none" }]],
+      lines: route[route.length - 1]["visited"].concat(
+        route[route.length - 1]["visited"][0]
+      ),
+    });
+    this.forceUpdate();
+  }
 
-    reset(cities) {
-        cities.map((city) => {
-            city.isRoute = false;
-        })
+  render() {
+    const { cities, seeking, lines, ANIMATION_DELAY, NUM_POINTS } = this.state;
 
-        this.setState({ cities: cities, lines: [] })
-    }
-
-    async displayPath(route) {
-        console.log(route)
-        for (const step of route.slice(0, -1)) {
-            let visited = step['visited']
-            
-            visited.forEach((e) => {
-                e.isRoute = true;
-            })
-
-            let unvisited = step['unvisited']
-            this.setState({ cities: visited.concat(unvisited), lines: visited })
-
-            for (let i = 0; i < step['active'].length; i++) {
-                // console.log(step['active'][i])
-                step['active'][i].isActive = true;
-                step['active'][i].isRoute = false;
-                for (let j = 0; j < step['seeking'][i].length; j++) {
-                    this.setState({ seeking: [step['active'][i], step['seeking'][i][j]] })
-                    await timer(this.state.ANIMATION_DELAY);
-                }
-                step['active'][i].isActive = false;
-                step['active'][i].isRoute = true;
+    // console.log(seeking);
+    return (
+      <div className="App">
+        <div className="interface">
+          <div className="algoButtons">
+            <h5>Algorithms</h5>
+            <ul>
+              <li>
+                <button
+                  onClick={() => this.displayPath(nearestNeighbour(cities))}
+                >
+                  Nearest Neighbour
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => this.displayPath(nearestInsertion(cities))}
+                >
+                  Nearest Insertion
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => this.displayPath(cheapestInsertion(cities))}
+                >
+                  Cheapest Insertion
+                </button>
+              </li>
+              <li>
+                <button>Temp</button>
+              </li>
+              <li>
+                <button>Temp</button>
+              </li>
+              <li>
+                <button>Temp</button>
+              </li>
+              <li>
+                <button>Temp</button>
+              </li>
+              <li>
+                <button>Temp</button>
+              </li>
+            </ul>
+          </div>
+          <ul>
+            <li>
+              <h5>Settings</h5>
+            </li>
+            <li>
+              <button onClick={() => this.begin()}>Generate New Array</button>
+            </li>
+            <li>
+              <span>Animation Delay</span>
+              <Slider
+                defaultValue={ANIMATION_DELAY}
+                min={5}
+                step={5}
+                max={100}
+                graduated
+                progress
+                value={ANIMATION_DELAY}
+                onChange={(value) => {
+                  this.setState({ ANIMATION_DELAY: value });
+                }}
+              />
+            </li>
+            <li>
+              <span>City Count</span>
+              <Slider
+                defaultValue={NUM_POINTS}
+                min={3}
+                step={1}
+                max={50}
+                graduated
+                progress
+                value={NUM_POINTS}
+                onChange={(value) => {
+                  this.setState({ NUM_POINTS: value });
+                  this.begin();
+                }}
+              />
+            </li>
+          </ul>
+        </div>
+        <div>
+          {cities.map((city) => {
+            return (
+              <City
+                key={city.className}
+                isRoute={city.isRoute}
+                isActive={city.isActive}
+                className={city.className}
+                x={city.x}
+                y={city.y}
+              />
+            );
+          })}
+          {seeking[1].map((seekCity, idx) => {
+            return seeking[0].map((activeCity, idx) => {
+              return (
+                <LineTo
+                  key={idx}
+                  from={`${activeCity.className}`}
+                  to={`${seekCity.className}`}
+                  borderColor="lightgrey"
+                />
+              );
+            });
+          })}
+          {lines.map((line, idx, lines) => {
+            let end;
+            if (idx === lines.length - 1) {
+              end = line;
+            } else {
+              end = lines[idx + 1];
             }
-        }
-
-        this.setState({ cities: route[route.length - 1]['visited'], 
-            seeking: [{className: "none"}, [{className: "none"}]], 
-            lines: route[route.length - 1]['visited'].concat(route[route.length - 1]['visited'][0]) })
-        this.forceUpdate()
-    }
-
-
-    render() {
-        const { cities, seeking, lines, ANIMATION_DELAY, NUM_POINTS } = this.state;
-
-        return (
-            <div className="App">
-                <div className="interface">
-                    <div className="algoButtons">
-                        <h5>Algorithms</h5>
-                        <ul>
-                            <li><button onClick={() => this.displayPath(nearestNeighbour(cities))}>Nearest Neighbour</button></li>
-                            <li><button onClick={() => this.displayPath(nearestInsertion(cities))}>Nearest Insertion</button></li>
-                            <li><button>Temp</button></li>
-                            <li><button>Temp</button></li>
-                            <li><button>Temp</button></li>
-                            <li><button>Temp</button></li>
-                            <li><button>Temp</button></li>
-                            <li><button>Temp</button></li>
-                        </ul>
-                    </div>
-                    <ul>
-                        <li><h5>Settings</h5></li>
-                        <li><button onClick={() => this.begin()}>Generate New Array</button></li>
-                        <li><span>Animation Delay</span><Slider defaultValue={ANIMATION_DELAY} min={5} step={5}
-                            max={100} graduated progress value={ANIMATION_DELAY}
-                            onChange={value => {
-                                this.setState({ ANIMATION_DELAY: value });
-                            }} />
-                        </li>
-                        <li><span>City Count</span><Slider defaultValue={NUM_POINTS} min={3} step={1}
-                            max={50} graduated progress value={NUM_POINTS} 
-                            onChange={value => {
-                                this.setState({ NUM_POINTS: value });
-                                this.begin();
-                            }} />
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    {cities.map((city) => {
-                        return (
-                            <City
-                                key={city.className}
-                                isRoute={city.isRoute}
-                                isActive={city.isActive}
-                                className={city.className}
-                                x={city.x}
-                                y={city.y}
-                            />
-                        );
-                    })}
-                    {seeking[1].map((city, idx) => {
-                        return (
-                            <LineTo 
-                                key={idx}
-                                from={`${seeking[0].className}`}
-                                to={`${city.className}`}
-                                borderColor="lightgrey"
-                            />
-                        );
-                    })}
-                    {lines.map((line, idx, lines) => {
-                        let end;
-                        if (idx === lines.length - 1) {
-                            end = line;
-                        } else {
-                            end = lines[idx + 1]
-                        }
-                        return (
-                            <LineTo
-                                key={idx}
-                                from={`${line.className}`}
-                                to={`${end.className}`}
-                                borderColor="turquoise"
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        );
-    }
+            return (
+              <LineTo
+                key={idx}
+                from={`${line.className}`}
+                to={`${end.className}`}
+                borderColor="turquoise"
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 function randomCoords(min, max) {
-    let x = Math.floor(Math.random() * (max - min + 1) + min);
-    let y = Math.floor(Math.random() * (max - min + 1) + min);
-    return [x, y];
-  }
-  
-  function distance(a, b) {
-    const deltaX = a.x - b.x;
-    const deltaY = a.y - b.y;
-    return Math.sqrt(deltaX ** 2 + deltaY ** 2);
-  }
-  
+  let x = Math.floor(Math.random() * (max - min + 1) + min);
+  let y = Math.floor(Math.random() * (max - min + 1) + min);
+  return [x, y];
+}
+
+function distance(a, b) {
+  const deltaX = a.x - b.x;
+  const deltaY = a.y - b.y;
+  return Math.sqrt(deltaX ** 2 + deltaY ** 2);
+}
